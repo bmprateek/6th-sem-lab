35addf2332d514911616933be58fa46c31a7bd89
bmprateek <bmprateek1511@gmail.com>
2022-06-27 02:28:49 +0530

    update

---
+++ CD lab/cie/cd_programs/q5/file.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+ #include<stdlib.h>
+ void main() 
+{ 
+int a=56; 
+a=1b; 
+a=a+5h; 
+a=a+4.5+5. 6.6; }
---
+++ CD lab/cie/cd_programs/q6/file.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ #include <stdlib.h>
+ #include <conio.h>
+void main() 
+{ int a,b=78,g="78",,; 
+float c=5.6,h="fg";
+ sa=5; 
+a=a+b; 
+printf("\n "); }
