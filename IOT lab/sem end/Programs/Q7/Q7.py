6e420ab1d62a7c1113a291891717d46f0718793c
bmprateek <55629272+bmprateek@users.noreply.github.com>
2022-08-12 22:11:22 +0530

    Update Q7.py

--- IOT lab/sem end/Programs/Q7/Q7.py
+++ IOT lab/sem end/Programs/Q7/Q7.py
@@ -4,11 +4,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(17 , GPIO.OUT)
 num = int(input("Enter a number"))
 if num == 1:
-	GPIO.ouput(17 , GPIO.high)
+	GPIO.ouput(17 , GPIO.HIGH)
 	time.sleep(1)
 	while(true):
 		num1 = int(input())
 			if num1==0:
-				GPIO.output(17 , GPIO.high)
+				GPIO.output(17 , GPIO.LOW)
 				break;
 GPIO.cleanup()
